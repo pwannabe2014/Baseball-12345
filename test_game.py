78d401113d1042601f717776ea579fb8fb3a2239
dhkang <pwannabe2014@gmail.com>
2025-07-31 14:02:49 +0900

    [feature] p.27

--- test_game.py
+++ test_game.py
@@ -1,4 +1,9 @@
 import pytest
 
-def test_game():
-    pass
+from game import Game
+
+
+def test_exception_when_input_is_none():
+    game = Game()
+    with pytest.raises(TypeError):
+        game.guess(None)
